f42bcee0b00efd48d241252cd0adbbffd69d24a1
Mitchell Cooper <mitchell.l.cooper@disney.com>
2022-02-28 16:57:25 -0500

    Remove provider block

--- terraform.tf
+++ terraform.tf
@@ -11,11 +11,3 @@ terraform {
   }
   required_version = ">= 1.1.3"
 }
-
-provider "azurerm" {
-  features {}
-  alias                      = "image_gallery"
-  subscription_id            = "c7e0617f-553f-4b8a-bf10-1a917a4aa9f4"
-  tenant_id                  = "56b731a8-a2ac-4c32-bf6b-616810e913c6"
-  skip_provider_registration = true
-}
